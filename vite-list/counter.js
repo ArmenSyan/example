8cbd0bcd603f4f5d02f5b83db648f8b2faf3310c
ArmenSyan <armen.sahakyan.ait@gmail.com>
2024-11-08 23:55:50 +0400

    counter.js -> add an addRow func and in html add src of counter

---
+++ vite-list/counter.js
@@ -0,0 +1,20 @@
+function addRow() {
+  let name = prompt("Նշեք ապրանքի անվանումը");
+  let gin = prompt("Նշեք ապրանքի արժեքը");
+  if (name == "" && gin == "") {
+    alert("Խնդրում ենք փորձել կրկին")
+  }
+  else {
+    let tbody = document.getElementsByClassName("tbody")[0];
+    let str = tbody.insertRow(-1);
+    let x = str.insertCell(0);
+    let y = str.insertCell(1);
+
+    x.innerHTML = name;
+    y.innerHTML = gin;
+
+
+    x.classLiist.add("name")
+    y.classLiist.add("cost")
+  }
+}
